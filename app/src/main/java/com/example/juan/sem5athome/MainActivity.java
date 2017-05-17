f7b6048fda35b840eed8e4e6c14c92a36cbd163f
Rubertavio <ruberpalomino1@gmail.com>
2017-05-16 23:46:08 -0500

    Version Final
    Corresponde a la version final del programa

--- app/src/main/java/com/example/juan/sem5athome/MainActivity.java
+++ app/src/main/java/com/example/juan/sem5athome/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.juan.sem5athome;
 
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -8,6 +9,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
     TextView texto;
@@ -25,13 +28,23 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         ll=(LinearLayout)findViewById(R.id.llSensores);
         sm=(SensorManager)getSystemService(SENSOR_SERVICE);
         sensor=sm.getDefaultSensor(sensor.TYPE_PROXIMITY);
-        sm.registerListener(this,sensor,sm.SENSOR_DELAY_NORMAL);
-
-
+        sm.registerListener(this,sensor,SensorManager.
+                SENSOR_DELAY_NORMAL);
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
+        String txt=event.values[0]+"";
+        texto.setText(txt);
+        float valor=event.values[0];
+
+        if (valor==0){
+            ll.setBackgroundColor(Color.CYAN);
+        }else{
+            ll.setBackgroundColor(Color.BLUE);
+        }
+
+
 
     }
 
